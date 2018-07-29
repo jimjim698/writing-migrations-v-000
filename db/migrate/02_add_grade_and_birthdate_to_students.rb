2a30646c60b2fab81c81b4a0a94e91d16a42813b
James Lotruglio <jimlotruglio@yahoo.com>
2018-07-29 22:29:08 +0000

    automatically backed up by learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,9 +1,9 @@
-class AddColumns < ActiveRecord::Migration
+class AddColumns < ActiveRecord::Migration[5.1]
 
   def add_column
     add_column :students, :grade, :integer
     add_column :students, :birthdate, :string
-  end 
+  end
 
 
 
